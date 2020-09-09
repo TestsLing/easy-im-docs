ee97310f4d3ead9bf709113709b55e90e2c49817
chenyingzhan <519203699@qq.com>
2020-09-09 14:33:00 +0800

    alter:隐藏博客和博文页面

--- .vuepress/config/nav/index.js
+++ .vuepress/config/nav/index.js
@@ -19,8 +19,8 @@ module.exports = {
     },
     // {text: '文档', link: '/views/im/', icon: 'reco-api'},
     { text: '常见问题', link: '/views/other/question', icon: 'reco-faq' },
-    { text: '博文', link: '/categories/blog/', icon: 'reco-blog'},
-    { text: '优秀博客', link: '/views/other/theme-example.html', icon: 'reco-category'},
+    // { text: '博文', link: '/categories/blog/', icon: 'reco-blog'},
+    // { text: '优秀博客', link: '/views/other/theme-example.html', icon: 'reco-category'},
     // { text: '订阅', link: 'https://vuepress-theme-reco.recoluan.com/rss.xml', icon: 'reco-rss'},
     { text: '留言板', link: '/views/other/messageBoard.html', icon: 'reco-suggestion'},
     { text: 'GitHub', link: 'https://github.com/TestsLing/easy-im', icon: 'reco-github'}
